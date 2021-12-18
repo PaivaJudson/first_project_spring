91c000cca0e9359fbf8d25dfb50b360a3171ab13
Judson Paiva <judson.paiva@hotmail.com>
2021-12-18 13:50:02 +0100

    Enviando menssagem com o model

--- src/main/java/com/judson/paiva/HomeController.java
+++ src/main/java/com/judson/paiva/HomeController.java
@@ -1,13 +1,22 @@
 package com.judson.paiva;
 
 import org.springframework.stereotype.Controller;
+<<<<<<< HEAD
+=======
+import org.springframework.ui.Model;
+>>>>>>> Enviando menssagem com o model
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
     @RequestMapping("/")
+<<<<<<< HEAD
     public String homeApp(){
+=======
+    public String homeApp(Model model){
+        model.addAttribute("messagem", "Essa foi uma menssagem injectada");
+>>>>>>> Enviando menssagem com o model
         return "index";
     }
 
