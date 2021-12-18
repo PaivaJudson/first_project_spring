3ff42533ed8a1fa829999197761d7d21c19d43e9
Judson Paiva <judson.paiva@hotmail.com>
2021-12-18 13:50:02 +0100

    Primeiro commit

---
+++ src/test/java/com/judson/paiva/FirstProjectSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.judson.paiva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstProjectSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
